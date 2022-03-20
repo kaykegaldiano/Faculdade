149a783096ae47229d65c664645185d8bbff1d13
Kayke Galdiano <kaykegaldiano@gmail.com>
2022-03-20 17:29:17 -0300

    criando a entity Curso

---
+++ src/Model/Curso.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Faculdade\Model;
+
+/**
+ * @Entity
+ * @Table(name="cursos")
+ */
+class Curso
+{
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private int $id;
+    /**
+     * @Column(type="string")
+     */
+    private string $nome;
+    /**
+     * @Column(type="string")
+     */
+    private string $descricao;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getDescricao(): string
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+}
