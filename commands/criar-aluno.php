7134a1bf113c4fd741ca4445817ea1402a20e531
Kayke Galdiano <kaykegaldiano@gmail.com>
2022-03-21 18:00:12 -0300

    .

--- commands/criar-aluno.php
+++ commands/criar-aluno.php
@@ -2,6 +2,7 @@
 
 use Faculdade\Infra\EntityManagerCreator;
 use Faculdade\Model\Aluno;
+use Faculdade\Model\Curso;
 use Faculdade\Model\Telefone;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -9,8 +10,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $entityManager = (new EntityManagerCreator())->getEntityManager();
 
 $aluno = new Aluno();
+
 $aluno->setNome($argv[1]);
 $aluno->setEmail($argv[2]);
+// $idCurso = $argv[3];
+$curso = $entityManager->find(Curso::class, 1);
+$aluno->setCurso($curso);
 
 for ($i = 3; $i < $argc; $i++) {
     $numeroTelefone = $argv[$i];
--- commands/criar-curso.php
+++ commands/criar-curso.php
@@ -5,13 +5,12 @@ use Faculdade\Model\Curso;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$entityManagerFactory = new EntityManagerCreator();
+$entityManager = $entityManagerFactory->getEntityManager();
+
 $curso = new Curso();
 $curso->setNome($argv[1]);
 $curso->setDescricao($argv[2]);
 
-$entityManagerFactory = new EntityManagerCreator();
-$entityManager = $entityManagerFactory->getEntityManager();
-
 $entityManager->persist($curso);
-
 $entityManager->flush();
